1ad3bb70c82289263c44b7ad6b015f7d5e852733
John <154112993+J0hn-C77n@users.noreply.github.com>
2025-08-06 15:32:14 +0300

    Changed start script so it works without proxychains by default

--- startzap.sh
+++ startzap.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-xpra start :100 --start-child="proxychains4 zaproxy -Xmx40G -config pscan.threads=16" --daemon=yes
+#xpra start :100 --start-child="proxychains4 zaproxy -Xmx40G -config pscan.threads=16" --daemon=yes
+xpra start :100 --start-child="zaproxy -Xmx40G -config pscan.threads=16" --daemon=yes
